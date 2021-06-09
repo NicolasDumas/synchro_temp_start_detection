26f01368e0f52bc286f372ed04311cd0e1ce27a8
NicolasDumas <67118529+NicolasDumas@users.noreply.github.com>
2021-06-09 09:58:45 +0200

    Update prepro_startDetection_auto.py
    Ma version de détection de départ la plus robuste

--- prepro_startDetection_auto.py
+++ prepro_startDetection_auto.py
@@ -13,15 +13,13 @@ def get_index(list_dict, vid_name):
             return i
 
 
-def extract_time_start(video_path, bip_ref_path="ref_bip_isolated.wav"):
-    # features of the ref
-    # extract short-term features using a 50msec non-overlapping windows
+def extract_time_start(video_path, bip_ref_path="ref_bip_isolated.wav", references_path = "ref_features_bip.npy"):
+    mt = np.load(references_path)
     fs, s_ref = aIO.read_audio_file(bip_ref_path)
     duration = len(s_ref) / float(fs)
     win, step = 0.05, 0.05
     win_mid, step_mid = duration, 0.5
-    mt_ref, st_ref, mt_n_ref = aFm.mid_feature_extraction(s_ref, fs, win_mid * fs, step_mid * fs,
-                                                          win * fs, step * fs)
+
     # extraction on the long signal
     my_clip1 = mp.VideoFileClip(video_path)
     fs = 44100
@@ -35,10 +33,35 @@ def extract_time_start(video_path, bip_ref_path="ref_bip_isolated.wav"):
     mt_long, st_long, mt_n_long = aFm.mid_feature_extraction(s_long, fs, win_mid * fs, step_mid * fs,
                                                              win * fs, step * fs)
 
-    # compute the distance and get the minimum
-    distances = np.linalg.norm(mt_long - mt_ref, axis=0)
-    time_start = np.argmin(distances) * duration_long / mt_long.shape[1]
-    return time_start
+    #normalization
+    mt_long = mt_long.T            
+    for i in range(len(mt_long)):
+        mt_long[i] = mt_long[i]/np.linalg.norm(mt_long[i])
+
+    temps_possible = []
+    
+    for i in range(len(mt)):
+        arg_min_dist = 0
+        min_dist = 1000
+        for j in range(len(mt_long)):
+            if np.linalg.norm(mt[i]-mt_long[j]) < min_dist :
+                arg_min_dist = j
+                min_dist = np.linalg.norm(mt[i]-mt_long[j])
+        temps_possible.append(arg_min_dist * duration_long / mt_long.shape[0])
+ 
+    median_time = np.median(temps_possible)
+    temps_possible_non_aberrant = []
+    aberration = 0.5
+    for i in range(len(temps_possible)):
+        if median_time - aberration <= temps_possible[i]:
+            if temps_possible[i] <= median_time + aberration :
+                temps_possible_non_aberrant.append(temps_possible[i])
+                         
+    # 0.11s de silence avant le bip dans les sons de références
+    start = np.median(temps_possible_non_aberrant) + 0.11
+
+    return start
+
 
 
 if __name__ == "__main__":
